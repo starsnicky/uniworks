81d1d272398a105f3913d039fff20cde161706b0
josia <git@github.com:starsnicky/uniworks.git>
2023-11-28 23:22:19 -0300

    Smartphone

---
+++ src/Smartphone.java
@@ -0,0 +1,14 @@
+public class Smartphone {
+    public static void main(String[] args) {
+        System.out.println();
+        Smartphone meuSmartphone = new Smartphone();
+        meuSmartphone.instalarApp("Instagram");
+    }
+    public void acessarInternet () {
+        System.out.println("Conectando");
+    }
+    public void instalarApp (String nomeApp){
+        System.out.println("Instalando o aplicativo: " + nomeApp);
+
+    }
+}
