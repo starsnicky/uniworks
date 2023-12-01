85c6b66cef0d906ce3dc8d08ae33a11c910304e9
Josiane-Rodrigues <151976901+Josiane-Rodrigues@users.noreply.github.com>
2023-11-30 22:54:55 -0300

    Update Celular.java

--- src/Celular.java
+++ src/Celular.java
@@ -1,18 +1,20 @@
-public class CelularComJogo extends  Celular{
+public class Celular{
     public static void main(String[] args) {
-        System.out.println("\nMetodo da Classe Celuar");
-        CelularComJogo meuCelularComJogo = new CelularComJogo();
-        meuCelularComJogo.tocarMuisica("Axe");
-        System.out.println("\nMetodo da Classe Celuar com Jogo");
-        meuCelularComJogo.iniciarJogo("Mario World");
-        meuCelularComJogo.pausarJogo();
+        Celular meuCelular = new Celular();
+        meuCelular.enviarMensagem("999999","Bom dia!");
+        meuCelular.tocarMusica("Rock");
+
+    }
+    public void  tocarMusica( String Musica){
+        System.out.println("Tocar"  +": " +  Musica);
     }
 
-    public void iniciarJogo(String nomeJogo) {
-        System.out.println("Iniciando o jogo: " + nomeJogo);
-    }
+    public void enviarMensagem(String numero, String mensagem) {
+        System.out.println(
 
-    public void pausarJogo() {
-        System.out.println("Jogo pausado");
-    }
+                "Enviando mensagem para " + numero + ": " + mensagem);
+
+       }
+
+
 }
