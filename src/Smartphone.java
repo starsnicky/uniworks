a5066b1f5c3d3d7beb9c70d5e2a9fe8d0281802e
starsnicky <151973164+starsnicky@users.noreply.github.com>
2023-11-30 21:29:52 -0300

    novo commit josi
    Versão 1 - Com a da Josiane

---
+++ src/Smartphone.java
@@ -0,0 +1,15 @@
+public class Smartphone {
+    public static void main(String[] args) {
+        System.out.println();
+        Smartphone meuSmartphone = new Smartphone();
+        meuSmartphone.instalarApp("Instagram");
+         meuSmartphone.acessarInternrt();
+    }
+    public void acessarInternet () {
+        System.out.println("Conectando");
+    }
+    public void instalarApp (String nomeApp){
+        System.out.println("Instalando o aplicativo: " + nomeApp);
+
+    }
+}
